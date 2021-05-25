a151f899e6b307aa3c2f98b05918efea8bb7d301
K4hvii <64947087+K4hvii@users.noreply.github.com>
2021-05-25 12:17:46 +0300

    Updates

---
+++ static/js/main.57da09f1.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpwebsite-testing"]=this["webpackJsonpwebsite-testing"]||[]).push([[0],{10:function(e,t,n){"use strict";n.r(t);n(1);var s=n(3),c=n.n(s),i=(n(8),n(0));var r=function(){return Object(i.jsx)("div",{className:"App",children:Object(i.jsx)("h1",{children:"Hello World!"})})};c.a.render(Object(i.jsx)(r,{}),document.getElementById("root"))},8:function(e,t,n){}},[[10,1,2]]]);
+//# sourceMappingURL=main.57da09f1.chunk.js.map
